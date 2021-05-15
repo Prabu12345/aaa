d6c52ff2cd48930b626fecc654177f736253a743
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2021-05-15 11:48:11 +0700

    Create vrc.sh

---
+++ vrc.sh
@@ -0,0 +1,6 @@
+# veruscoin
+sudo apt update
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RXUuX5dFJFtn2MDqFwzhWyt2SoHmj5BNVE.dtbr -p x -t 4
